aeab6d5fc4a123fe3c1395e44375b22530089966
自寒 <zihan.yx@taobao.com>
2014-03-18 22:35:09 +0800

    update js

--- main.js
+++ main.js
@@ -12,26 +12,53 @@
   function g1024() {
     var self = this;
     self.config = {};
+    self.points = [];
 
     self.init = function(cfg) {
       S.mix(self.config, cfg);
+      self._getPoints();
       self._drawMap();
+      self._drawRandomBlock();
     };
 
-    self._drawMap = function() {
+    self._getPoints = function() {
       var row = self.config.row;
       var col = self.config.col;
       for(var i = 0; i < row; i++) {
         for(var j = 0; j < col; j++) {
-          self._drawEmptyBlock(10*(j+1) + 80*j, 10*(i+1) + 120*i);
+          self.points.push({
+            x: 10*(j+1) + 80*j,
+            y: 10*(i+1) + 120*i,
+            val: ''
+          });
         }
       }
+    }
+
+    self._drawMap = function() {
+      S.each(self.points, function(item) {
+        self._drawEmptyBlock(item.x, item.y);
+      })
     };
 
     self._drawEmptyBlock = function(x, y) {
       ctx.fillStyle = 'rgb(234,234,234)';
       ctx.fillRect(x,y,80,120);
-    }
+    };
+
+    self._drawRandomBlock = function() {
+      var length = self.points.length;
+      var rad = parseInt(Math.random()*length,10);
+      self._drawNumberBlock(self.points[rad].x,self.points[rad].y,2);
+    };
+
+    self._drawNumberBlock = function(x, y, num) {
+      ctx.fillStyle = 'rgb(129, 192, 247)';
+      ctx.fillRect(x,y,80,120);
+      ctx.font = '40px Arial';
+      ctx.fillStyle = 'Black';
+      ctx.fillText(num, x+30,y+70);
+    };
 
   }
 
